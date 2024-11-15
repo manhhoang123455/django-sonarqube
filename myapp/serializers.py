d2feddcfec9e407118c9b6435ae4b2487cbb61e5
hoang.pnm <hoangpnm@vn.sprite.jp>
2024-11-15 09:18:00 +0700

    update

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 
 class UserSerializer(serializers.ModelSerializer):
